6071731b8be5ece5c8a758e2f9c18fd9549de605
VN <nagovicin2005@gmail.com>
2021-10-17 21:55:58 +0300

    FVer

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -5,18 +5,22 @@ int main()
 {
 
 	setlocale(LC_ALL, "Rus");
-	int x, c, R, i;
+	int x, c, i, f;
 	long double s;
 	s = 0;
+	c = 0;
+	f = 0;
 	std::cout << "Введите количество необходимых для вывода цифр после запятой(не больше 8):";
-	std::cin >> x, R;
-	for (i = 1; i < 1000000000; i++)
+	std::cin >> x;
+	for (i = 1; i < 1000000; i++)
 	{
-		if (pow(rand(R), 2) + pow(rand(R), 2) <= R)
+		c = pow(rand(), 2);
+		f = pow(rand(), 2);
+		if ((c + f) < pow(32768, 2))
 		{
 			s += 1;
 		}
 	}
-	std::cout << std::setprecision(x + 1) << (s / i) * 4;
+	std::cout << std::setprecision(x + 1) << ((s / i) * 4);
 	return 0;
 }
