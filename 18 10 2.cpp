7c332f60173274a17cfe0f0111a41c0777b7eafb
VN <nagovicin2005@gmail.com>
2021-10-19 14:21:40 +0300

    zad 18.10

---
+++ 18 10 2.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include <iomanip>
+#include <cmath>
+int main()
+{
+
+	setlocale(LC_ALL, "Rus");
+	int x, s,coun,r,c,del;
+	coun = 0;
+	c = 0;
+	char ch;
+	std::cout << "Введите число и нужную систему счисления:";
+	std::cin >> x>>s;
+	r = x;
+	while (r > 0)
+	{
+		r /= s;
+		coun++;
+	}
+	del = coun;
+	for (int i = 0; i < del; i++)
+	{
+		c = ((x / (pow(s, (coun - 1)))));
+		if (c % s < 10)
+		{
+			std::cout << (c % s);
+		}
+		else
+		{
+			std::cout << char((c % s)-10 + 'A');
+		}
+		coun--;
+	}
+	return 0;
+}
---
+++ 18.10 3.cpp
@@ -0,0 +1,104 @@
+﻿#include<iostream>
+#include <iomanip>
+#include <cmath>
+int main()
+{
+
+	setlocale(LC_ALL, "Rus");
+	int x, coun, max,s;
+	coun = 1000;
+	s = 0;
+	std::cout << "Введите исходное число>0:";
+	std::cin >> x;
+		s = x / 1000;
+		for (int i = 0; i < s; i++)
+		{
+			std::cout << "M";
+		}
+		x =x%1000;
+		s = x / 100;
+		if (s == 9)
+		{
+			std::cout << "CM";
+		}
+		if (s < 5)
+		{
+			if (s == 4)
+			{
+				std::cout << "CD";
+			}
+			else
+			{
+				for (int i = 0; i < s; i++)
+				{
+					std::cout << "C";
+				}
+			}
+		}
+		if (s >= 5 && s != 9)
+		{
+			s = x / 500;
+			for (int i = 0; i < s; i++)
+			{
+				std::cout << "D";
+			}
+		}
+		x = x % 100;
+		s = x / 10;
+		if (s == 9)
+		{
+			std::cout << "XC";
+		}
+		if (s < 5)
+		{
+			if (s == 4)
+			{
+				std::cout << "XL";
+			}
+			else
+			{
+				for (int i = 0; i < s; i++)
+				{
+					std::cout << "X";
+				}
+			}
+		}
+		if (s >= 5 && s != 9)
+		{
+			s = x / 50;
+			for (int i = 0; i < s; i++)
+			{
+				std::cout << "L";
+			}
+		}
+		x = x % 10;
+		s = x / 1;
+		if (s == 9)
+		{
+			std::cout << "IX";
+		}
+		if (s < 5)
+		{
+			if (s == 4)
+			{
+				std::cout << "IV";
+			}
+			else
+			{
+				for (int i = 0; i < s; i++)
+				{
+					std::cout << "I";
+				}
+			}
+		}
+		if(s>=5 && s!=9)
+		{
+			s = x / 5;
+			for (int i = 0; i < s; i++)
+			{
+				std::cout << "V";
+			}
+		}
+		x = x % 1;
+	return 0;
+}
