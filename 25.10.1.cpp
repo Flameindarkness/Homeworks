8a075d3d6fef570f9221b1e3adb2cb654830196d
VN <nagovicin2005@gmail.com>
2021-11-03 21:15:12 +0300

    add 25.10 1) 2) HW

---
+++ 25.10.1.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include <iomanip>
+#include <cmath>
+int main()
+{
+
+	setlocale(LC_ALL, "Rus");
+	int d, m,y,a,b,r;
+	std::cout << "Введите день и месяц:";
+	std::cin >> d >> m;
+	y = 0;
+	r = 0;
+	a = (14-m)/12;
+	b = m+12*a-2;
+	for (int i = 2021; i < 9999; i++)
+	{
+		y = i - a;
+		r = (d + y + y / 4 - y / 100 + y / 400 + 31 * b / 12) % 7;
+		if (r == 5)
+		{
+			std::cout << i;
+			break;
+		}
+	}
+	
+	return 0;
+}
---
+++ 25.10.2.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include <iomanip>
+#include <cmath>
+int main()
+{
+
+	setlocale(LC_ALL, "Rus");
+	int d, m,y,a,b,r,coun=0;
+	std::cout << "Вероятность 1/7"<<"\n";
+	y = 0;
+	r = 0;
+	for (int i = 1901; i < 2001; i++)
+	{
+		for (int j = 0; j < 12; j++)
+		{
+			a = (14 - j) / 12;
+			b = j + 12 * a - 2;
+			y = i - a;
+			r = (13 + y + y / 4 - y / 100 + y / 400 + 31 * b / 12) % 7;
+			if (r == 5)
+			{
+				coun++;
+			}
+		}
+	}
+	std::cout << coun;
+	return 0;
+}
