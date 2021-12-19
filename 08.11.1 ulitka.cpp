5ea0c8b82abf4606a334d5c61a5ba80b08aaf998
VN <nagovicin2005@gmail.com>
2021-12-20 02:27:15 +0300

    add a small par of HW

---
+++ 08.11.1 ulitka.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+
+int main()
+{
+	int  row, col,k=1;
+	std::cin >> row >> col;
+	int **A = new int *[row];
+	for (int  i = 0; i < row; ++i) { A[i] = new int[col]; }
+	int cb = 0, ce = col-1, rb = 0, re = row - 1, n = 0, m = 0;
+	A[0][0] = k;
+	for (; rb <= re;) {
+		for (; n < ce;) A[m][++n] = ++k;
+		++rb;
+		for (; m < re;) A[++m][n] = ++k;
+		--ce;
+		for (; n > cb;) A[m][--n] = ++k;
+		--re;
+		for (; m > rb;) A[--m][n] = ++k;
+		++cb;
+	}
+	A[m][n] = k;
+	for (int i = 0; i < row; ++i) {
+		for (int j = 0; j < col; ++j) {
+			if(j!=col-1)
+			std::cout << A[i][j] << ' ';
+			else
+			std::cout << A[i][j]<<std::endl;
+		}
+	}
+	for (int i = 0; i < row; ++i) delete[] A[i];
+	delete[] A;
+	return 0;
+}
---
+++ 15.11.1 det matrix(unworking).cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+using namespace std;
+void GetMatr(double** mas, double** p, double i, double j, double m) {
+    int ki, kj, di, dj;
+    di = 0;
+    for (ki = 0; ki < m - 1; ki++) {
+        if (ki == i) di = 1;
+        dj = 0;
+        for (kj = 0; kj < m - 1; kj++) {
+            if (kj == j) dj = 1;
+            p[ki][kj] = mas[ki + di][kj + dj];
+        }
+    }
+}
+double Det(double** mas, double m) {
+    int i, j, d, k, n;
+    double** p;
+    p = new double* [m];
+    for (i = 0; i < m; i++)
+        p[i] = new double[m];
+    j = 0; d = 0;
+    k = 1;
+    n = m - 1;
+    if (m < 1) cout << "Определитель вычислить невозможно!";
+    if (m == 1) {
+        d = mas[0][0];
+        return(d);
+    }
+    if (m == 2) {
+        d = mas[0][0] * mas[1][1] - (mas[1][0] * mas[0][1]);
+        return(d);
+    }
+    if (m > 2) {
+        for (i = 0; i < m; i++) {
+            GetMatr(mas, p, i, 0, m);
+            d = d + k * mas[i][0] * Det(p, n);
+            k = -k;
+        }
+    }
+    return(d);
+}
+int main() {
+    int m, i, j;
+    double d;
+    double** mas;
+    system("chcp 1251");
+    system("cls");
+    cout << "Введите размерность квадратной матрицы: ";
+    cin >> m;
+    mas = new double* [m];
+    for (i = 0; i < m; i++) {
+        mas[i] = new double[m];
+        for (j = 0; j < m; j++) {
+            cin >> mas[i][j];
+        }
+    }
+    d = Det(mas, m);
+    cout << "Определитель матрицы равен " << d;
+    cin.get(); cin.get();
+    return 0;
+}
