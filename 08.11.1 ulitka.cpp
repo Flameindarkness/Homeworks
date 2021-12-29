20ae6d9b6b5e663ec8ec4e6123a2991868d97334
Univer_acc_VN <92276913+Flameindarkness@users.noreply.github.com>
2021-12-29 11:37:05 +0300

    Update 08.11.1 ulitka.cpp

--- 08.11.1 ulitka.cpp
+++ 08.11.1 ulitka.cpp
@@ -1,34 +1,45 @@
-﻿#include <iostream>
-
+#include <iostream>
 
 int main()
 {
-	int  row, col,k=1;
+	setlocale(LC_ALL, "Russian");
+	int  row, col, k = 1;
+	std::cout << "Введите размерность матрицы:";
 	std::cin >> row >> col;
-	int **A = new int *[row];
-	for (int  i = 0; i < row; ++i) { A[i] = new int[col]; }
-	int cb = 0, ce = col-1, rb = 0, re = row - 1, n = 0, m = 0;
+	int** A = new int* [row];
+	for (int i = 0; i < row; ++i) { A[i] = new int[col]; }
+	int cb = 0, ce = col - 1, rb = 0, re = row - 1, n = 0, m = 0;
 	A[0][0] = k;
 	for (; rb <= re;) {
-		for (; n < ce;){if(k<=row*col) A[m][++n] = ++k;}
+		if (k == row * col)
+			break;
+		for (; n < ce;) A[m][++n] = ++k;
 		++rb;
-		for (; m < re;){if(k<=row*col) A[++m][n] = ++k;}
+		if (k == row * col)
+			break;
+		for (; m < re;) A[++m][n] = ++k;
 		--ce;
-		for (; n > cb;){if(k<=row*col) A[m][--n] = ++k;}
+		if (k == row * col)
+			break;
+		for (; n > cb;) A[m][--n] = ++k;
 		--re;
-		for (; m > rb;){if(k<=row*col) A[--m][n] = ++k;}
+		if (k == row * col)
+			break;
+		for (; m > rb;) A[--m][n] = ++k;
 		++cb;
+		if (k == row * col)
+			break;
 	}
 	A[m][n] = k;
 	for (int i = 0; i < row; ++i) {
 		for (int j = 0; j < col; ++j) {
-			if(j!=col-1)
-			std::cout << A[i][j] << ' ';
+			if (j != col - 1)
+				std::cout << A[i][j] << ' ';
 			else
-			std::cout << A[i][j]<<std::endl;
+				std::cout << A[i][j] << std::endl;
 		}
 	}
-	for (int i = 0; i < row; ++i) delete[] A[i];
+	for (int i = 0; i < row; ++i) { delete[] A[i]; }
 	delete[] A;
 	return 0;
 }
