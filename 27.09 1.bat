9b4b4bd3892e8c628e65e55df8660fda057c4c62
Univer_acc_VN <92276913+Flameindarkness@users.noreply.github.com>
2021-12-29 15:56:27 +0300

    Update 27.09 1.bat

--- 27.09 1.bat
+++ 27.09 1.bat
@@ -1,5 +1,12 @@
 @echo off
 for /L %%A in (33,1,255) do (
-    cmd /C exit %%A
-    call echo %%A - %%^=ExitCodeAscii%%
+    call :_tochar %%A
+    cmd /c exit %%A
 )
+exit /b
+:_tochar
+setlocal
+cmd /c exit %1
+echo %1- "%=exitcodeAscii%"
+exit /b
+
